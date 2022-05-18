6cfee40bfec48214e573e260103f0172711e5537
Max-munene <munenemaxwell2@gmail.com>
2022-05-18 13:16:06 +0300

    Delete

--- src/App.js
+++ src/App.js
@@ -16,10 +16,18 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch(type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite){
+        return{
+          ...state,
+          currentOperand: payload.digit,
+          overwrite:false,
+        }
+      }
       if (payload.digit === "0" && state.currentOperand === "0") 
       return state
       if (payload.digit === "." && state.currentOperand.includes(".")) 
       return state
+
       return  {
         ...state, currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       }
@@ -50,13 +58,34 @@ function reducer(state, { type, payload }) {
       }
     case ACTIONS.CLEAR:
       return{}
-
+    
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+      return{
+        ...state,
+        overwrite: false,
+        currentOperand: null,
+      }
+    }
+      if (state.currentOperand == null) return state
+      if (state.currentOperand.length === 1){
+        return {
+          ...state,
+          currentOperand: null
+        }
+      }
+      return{
+        ...state,
+        currentOperand: state.currentOperand.slice(0,-1)
+      }
+      
     case ACTIONS.EVALUATE:
       if (state.operation == null || state.currentOperand == null || state.previousOperand == null){
         return state
       }
       return {
         ...state,
+        overwrite: true,
         previousOperand: null,
         operation: null,
         currentOperand: evaluate(state)
@@ -100,7 +129,7 @@ function App() {
         <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two" onClick={() => dispatch({type: ACTIONS.CLEAR })}>AC</button>
-      <button>DEL</button>
+      <button onClick={() => dispatch({type: ACTIONS.DELETE_DIGIT })}>DEL</button>
       <OperationButton operation="÷" dispatch={dispatch}/>
       <DigitButton digit="1" dispatch={dispatch}/>
       <DigitButton digit="2" dispatch={dispatch}/>
